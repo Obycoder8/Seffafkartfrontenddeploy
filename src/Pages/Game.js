57453b1e7dd8dfce581892c74aed5ac43d00a13f
Obycoder8 <90784287+Obycoder8@users.noreply.github.com>
2023-01-04 01:17:44 +0300

    final

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -7,20 +7,22 @@ import StarRating from '../components/StarRating';
 
 
 function Game(props) {
+  const currentDate = new Date();
   const location = useLocation();
   const [match_data, setMatchData] = useState("");
   const [admin_acts, setAdminActs] = useState([]);
+  const [match_comments, setMatchComments] = useState([]);
+  const [comment,setComment] = useState("");
   const match_id = location.state;
   const user = useSelector((state) => state.user.currentUser);
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const isAdmin = useSelector((state) => state.user.admin);
   let text = ""
-  let posRate = 0
-  let negRate = 0
-  const [pR, setPosRate] = useState();
-  const [nR, setNegRate] = useState();
-  const [posclick, setPosClick] = useState(false);
-  const [negclick, setNegClick] = useState(false);
-  const [ctr, setCtr] = useState(0);
-  const [voted, setVoted] = useState({});
+  let comments = "" 
+  let adminActs = ""
+
+  console.log("user")
+  console.log(user);
 
   const [r1,setR1] = useState(0);
   const [r2,setR2] = useState(0);
@@ -33,56 +35,72 @@ function Game(props) {
    * @param  {integer} id id of the administrative act
    * @param  {string} rate rate type, pos or neg, of the adminisrative act
    */
-  const handleRate = async (id,rate) => {
-    Axios.get(`http://localhost:3001/api/ratingadminact/act_id=${id}&user_id=${user[0].id}`)
-    .then((response) => {
-        console.log(response.data,"data")
-        if(response.data.length !== 0) {
-          if(response.data[0].rate_type === rate) {
-            console.log("del time")
-            Axios.delete(`http://localhost:3001/api/deleterate/${response.data[0].id}`)
-            .then((response) => {
-                alert("deleted");
-            });
-          } else {
-            Axios.put("http://localhost:3001/api/updaterateadminact", {
+  const handleRate = async (act_id,rate) => {
+    if (isLogin === false) {
+      alert("You should logged in first to rate an act!");
+    }
+    else if (isAdmin === true) {
+      alert("You are an admin!");
+    }
+    else {
+      Axios.get(`https://team20-seffafkart.herokuapp.com/api/ratingadminact/act_id=${act_id}&user_id=${user[0].id}`)
+      .then((response) => {
+          console.log(response.data,"data")
+          if(response.data.length !== 0) {
+            if(response.data[0].rate_type === rate) {
+              console.log("del time")
+              Axios.put(`https://team20-seffafkart.herokuapp.com/api/deleterate/${response.data[0].id}`, {
+                act_id: act_id,
+                rate_type: rate
+              }).then((response) => {
+                  alert("deleted");
+              });
+            } else {
+              Axios.put("https://team20-seffafkart.herokuapp.com/api/updaterateadminact", {
+                user_id:user[0].id,
+                rate_type: rate,
+                act_id: act_id
+              }).then((response) => {
+                  alert("successfully voted act");
+              });
+            }
+          } 
+          else {
+            alert("not rated before");
+            Axios.post("https://team20-seffafkart.herokuapp.com/api/rateadminact", {
               user_id:user[0].id,
               rate_type: rate,
-              act_id: id
+              act_id: act_id
             }).then((response) => {
                 alert("successfully voted act");
             });
           }
-        } 
-        else {
-          alert("not rated before");
-          Axios.post("http://localhost:3001/api/rateadminact", {
-            user_id:user[0].id,
-            rate_type: rate,
-            act_id: id
-          }).then((response) => {
-              alert("successfully voted act");
-          });
-        }
-    });
+      });
+    }
   }
 
   useEffect(() => {
-    Axios.get(`http://localhost:3001/api/football_match/${match_id}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/football_match/${match_id}`)
     .then(res => {
         setMatchData(res.data[0])
     }).catch(err => console.log(err))
 
-    Axios.get(`http://localhost:3001/api/admin_acts/match_id=${match_id}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/admin_acts/match_id=${match_id}`)
     .then(res => {
         setAdminActs(res.data)
+        
+    }).catch(err => console.log(err))
+
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/comment_match/match_id=${match_id}`)
+    .then(res => {
+        setMatchComments(res.data)
     }).catch(err => console.log(err))
 
     console.log("ne zaman")
-}, [])
+},)
 
   useEffect(() => {
-    Axios.get(`http://localhost:3001/api/ratingreferee/match_id=${match_id}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/ratingreferee/match_id=${match_id}`)
     .then((response) => {
       console.log(response.data)
       response.data.forEach(element => {
@@ -109,13 +127,13 @@ function Game(props) {
    * @param  {integer} data rate value
    */
   const getRateRefereeOne = (data) => {
-    Axios.get(`http://localhost:3001/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${1}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${1}`)
     .then((response) => {
         console.log(response.data,"data respoms")
         console.log(response)
         if(response.data.length !== 0) {
           alert("rated before")
-          Axios.put("http://localhost:3001/api/updateratereferee", {
+          Axios.put("https://team20-seffafkart.herokuapp.com/api/updateratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -126,7 +144,7 @@ function Game(props) {
         } 
         else {
           alert("not rated before");
-          Axios.post("http://localhost:3001/api/ratereferee", {
+          Axios.post("https://team20-seffafkart.herokuapp.com/api/ratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -140,13 +158,13 @@ function Game(props) {
   }
 
   const getRateRefereeTwo = (data) => {
-    Axios.get(`http://localhost:3001/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${2}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${2}`)
     .then((response) => {
         console.log(response.data,"data respoms")
         console.log(response)
         if(response.data.length !== 0) {
           alert("rated before")
-          Axios.put("http://localhost:3001/api/updateratereferee", {
+          Axios.put("https://team20-seffafkart.herokuapp.com/api/updateratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -157,7 +175,7 @@ function Game(props) {
         } 
         else {
           alert("not rated before");
-          Axios.post("http://localhost:3001/api/ratereferee", {
+          Axios.post("https://team20-seffafkart.herokuapp.com/api/ratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -171,13 +189,13 @@ function Game(props) {
   }
 
   const getRateRefereeThree = (data) => {
-    Axios.get(`http://localhost:3001/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${3}`)
+    Axios.get(`https://team20-seffafkart.herokuapp.com/api/ratingreferee/match_id=${match_id}&user_id=${user[0].id}&referee_num=${3}`)
     .then((response) => {
         console.log(response.data,"data respoms")
         console.log(response)
         if(response.data.length !== 0) {
           alert("rated before")
-          Axios.put("http://localhost:3001/api/updateratereferee", {
+          Axios.put("https://team20-seffafkart.herokuapp.com/api/updateratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -188,7 +206,7 @@ function Game(props) {
         } 
         else {
           alert("not rated before");
-          Axios.post("http://localhost:3001/api/ratereferee", {
+          Axios.post("https://team20-seffafkart.herokuapp.com/api/ratereferee", {
             match_id: match_id,
             user_id:user[0].id,
             value: data,
@@ -201,6 +219,25 @@ function Game(props) {
     setRefereeCTR(refereeCTR + 1)
   }
 
+  const makeComment = () => {
+    if (isLogin === false) {
+      alert("You should logged in first to comment on a game!");
+    }
+    else if (isAdmin === true) {
+      alert("You are an admin!");
+    }
+    else {
+        Axios.post("https://team20-seffafkart.herokuapp.com/api/makecomment", {
+          content: comment,
+          username: user[0].username,
+          date: currentDate.toDateString(),
+          match_id: match_id
+        }).then((err) => {
+          alert("Successfully commented");
+         });  
+      };
+    }
+  
   if(match_data.main_referee != null){
     text = <div className='referee-container'>
               <text className='referee-text'>Main Official: {match_data.main_referee}</text>
@@ -221,29 +258,49 @@ function Game(props) {
     text = <div className='referee-container'>
       <text className='referee-text'>Referees not yet assigned.</text>
     </div>
-}
+  }
 
-const adminActs = admin_acts.map((act, index) => {
-  return (
-  <div className="act-box-game">
-    <span className='act-box-time'>{act.act_time}</span>
-    <span className='act-box-info'>
-      {act.act_info}
-    </span>
-    <div className='act-box-bottom'>
-      <button className="act-box-comments">
-      <Link className="text" style={{textDecoration:'none'}} to="/comments" state={act.admin_act_id}>Comment</Link>
-      </button>
-      <div className='act-box-rate'>
-      <label>{act.act_rate_pos}</label>
-      <i className="postIconLike fas fa-thumbs-up" onClick={() => handleRate(act.admin_act_id,"pos")}></i>
-      <i className="postIconDislike fas fa-thumbs-down" onClick={() => handleRate(act.admin_act_id,"neg")}></i>
-      <label>  {act.act_rate_neg}</label>
+if (admin_acts.length === 0) {
+  adminActs = <label>No administrative acts are given yet..</label>
+}
+else {
+  adminActs = admin_acts.map((act, index) => {
+    return (
+    <div className="act-box-game">
+      <span className='act-box-time'>{act.act_time}</span>
+      <span className='act-box-info'>
+        {act.act_info}
+      </span>
+      <div className='act-box-bottom'>
+        <button className="act-box-comments">
+        <Link className="text" style={{textDecoration:'none'}} to="/comments" state={act.admin_act_id}>Comment</Link>
+        </button>
+        <div className='act-box-rate'>
+        <label>{act.act_rate_pos}</label>
+        <i className="postIconLike fas fa-thumbs-up" onClick={() => handleRate(act.admin_act_id,"pos")}></i>
+        <i className="postIconDislike fas fa-thumbs-down" onClick={() => handleRate(act.admin_act_id,"neg")}></i>
+        <label>  {act.act_rate_neg}</label>
+        </div>
       </div>
     </div>
-  </div>
-  )});
+    )});
+}
 
+if (match_comments.length === 0) {
+  comments = <label className='no-comment'>No comments are given yet..</label>
+}
+else {
+  comments = match_comments.map((comm,index) => {
+    return (
+        <div className='comment-box-game'>
+          <div className='comment-box-game-top'>
+            <label className='comment-box-game-user'> {comm.username}:</label>
+            <label className='comment-box-game-time'>{comm.date}</label>
+          </div>
+          <div className='comment-box-game-content'>{comm.content}</div>
+        </div>
+    )});
+}
 
   return (
     <>
@@ -268,6 +325,18 @@ const adminActs = admin_acts.map((act, index) => {
             <text className='referee-title'>Referees</text>
             {text}
           </div >
+        </div>
+        
+        <div className='comment-container'> 
+            <label className='comment-container-text'>Comments on game..</label>
+            <div className='make-comment'>
+              <label className='make-comment-text'>You can leave your comment below</label>
+              <div className='make-comment-comment'>
+                <input className='make-comment-input' placeholder='Make comment....' onChange={(e) => {setComment(e.target.value);}}/>
+              </div>
+              <button className='make-comment-btn' onClick={makeComment}>Comment</button>
+            </div>
+            {comments}
         </div>
     </div>
     </>
